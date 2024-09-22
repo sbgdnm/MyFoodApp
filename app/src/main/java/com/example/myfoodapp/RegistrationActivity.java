5c26ef437496834edc859bb76ec72cef02aa0217
Samat Bagdan <sbm.overlord@gmail.com>
2024-09-23 00:13:26 +0600

    Welcome,Registration,Login activity

--- app/src/main/java/com/example/myfoodapp/RegistrationActivity.java
+++ app/src/main/java/com/example/myfoodapp/RegistrationActivity.java
@@ -1,6 +1,8 @@
 package com.example.myfoodapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,9 @@ public class RegistrationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(RegistrationActivity.this , LoginActivity.class));
     }
 }
--- app/src/main/java/com/example/myfoodapp/WelcomeActivity.java
+++ app/src/main/java/com/example/myfoodapp/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.myfoodapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class WelcomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void register(View view) {
+        startActivity(new Intent(WelcomeActivity.this,RegistrationActivity.class));
+    }
+
+    public void login(View view) {
+        startActivity(new Intent(WelcomeActivity.this , LoginActivity.class));
     }
 }
