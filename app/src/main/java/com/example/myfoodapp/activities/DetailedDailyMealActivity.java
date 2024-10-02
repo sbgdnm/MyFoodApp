4358f6d70e31c8f081364cbe50320a49d777169a
Samat Bagdan <sbm.overlord@gmail.com>
2024-10-02 19:31:05 +0600

    start create favourite fragment

--- app/src/main/java/com/example/myfoodapp/activities/DetailedDailyMealActivity.java
+++ app/src/main/java/com/example/myfoodapp/activities/DetailedDailyMealActivity.java
@@ -31,6 +31,7 @@ public class DetailedDailyMealActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_detailed_daily_meal);
 
         String type = getIntent().getStringExtra("type");
